62721363664359e46609d911e9229c746b2b7013
Eve <948832626@qq.com>
2021-04-24 17:23:00 +0800

    docs: 补充导航

--- src/app/page/container/container-03.component.ts
+++ src/app/page/container/container-03.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Anchor } from 'src/app/libs/content-fill/content-fill.interface';
 import { ContainerBaseComponent } from './container-base.component';
 
 @Component({
   template: `
-    <app-content-fill>
+    <app-content-fill [anchor]="anchor">
       <app-markdown [src]="appService.getPath('07', 'md')"></app-markdown>
       <app-markdown [src]="appService.getPath('05', 'html')"></app-markdown>
       <app-markdown [src]="appService.getPath('05', 'ts')"></app-markdown>
@@ -12,4 +13,19 @@ import { ContainerBaseComponent } from './container-base.component';
     </app-content-fill>
   `
 })
-export class Container03Component extends ContainerBaseComponent {}
+export class Container03Component extends ContainerBaseComponent {
+  anchor: Anchor[] = [
+    {
+      href: '#anchor001',
+      title: '概述'
+    },
+    {
+      href: '#anchor002',
+      title: 'takeUntil'
+    },
+    {
+      href: '#anchor003',
+      title: 'debounceTime'
+    }
+  ];
+}
