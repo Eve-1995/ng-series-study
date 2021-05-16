551d2f805840c9dbb8afa36816d5fa95765cb1c4
Eve <948832626@qq.com>
2021-05-16 17:17:57 +0800

    feat: 补充了一些内容

--- src/app/container/demo/demo-008/demo-008.component.ts
+++ src/app/container/demo/demo-008/demo-008.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-demo-008',
-  templateUrl: './demo-008.component.html'
+  templateUrl: './demo-008.component.html',
+  styleUrls: ['./demo-008.component.scss']
 })
 export class Demo008Component {
-  dateTime = new Date();
+  isRed = false;
 }
